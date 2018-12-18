5cabb859428a4335d4ec063b12e66231feae5483
nikolay-advolodkin <nikolay.advolodkin@saucelabs.com>
2018-12-18 12:39:09 -0500

    1st project

---
+++ AppiumTests/AppiumTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("AppiumTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("AppiumTests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("f023ed78-f9ab-4eb3-bfba-1664288d1013")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ AppiumTests/AppiumTests/RdcCheckWithMsTest.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Reflection;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Appium.Android;
+using OpenQA.Selenium.Remote;
+
+namespace AppiumTests
+{
+    [TestClass]
+    [TestCategory("MsTest")]
+    [TestCategory("Rdc")]
+    [TestCategory("Android")]
+
+    public class RdcCheckWithMsTest
+    {
+        [TestMethod]
+        public void RunRdcOnAndroid()
+        {
+            var capabilities = new DesiredCapabilities();
+            capabilities.SetCapability("platformName", "Android");
+            capabilities.SetCapability("platformVersion", "7.1.1");
+            //TODO first you must upload an app to Test Object so that you get your app key
+            capabilities.SetCapability("testobject_api_key", "0D6C044F19D0442BA1E11C3FF087F6A6");
+            capabilities.SetCapability("name", MethodBase.GetCurrentMethod().Name);
+            capabilities.SetCapability("newCommandTimeout", 90);
+
+            var rdcUrl = "https://us1.appium.testobject.com/wd/hub";
+            //TODO should be interchangeable to run on Windows/Mac
+            var driver = new AndroidDriver<IWebElement>(new Uri(rdcUrl), capabilities);
+            //var logEntries = driver.Manage().Logs.GetLog("driver");
+            driver.Navigate().GoToUrl("https://www.saucedemo.com");
+            driver.Quit();
+        }
+    }
+}
